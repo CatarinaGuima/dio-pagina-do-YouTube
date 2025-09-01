3298edfc14086ffb57bd5afffbb5b35b837b9e0e
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2025-08-31 23:11:07 -0300

    feat: Implementa a lista dos videos

---
+++ data/data.js
@@ -0,0 +1,42 @@
+const videos = [
+  {
+    "id": 1,
+    "thumbnail": "./assets/images/thumbnail1.png",
+    "title": "Geração Tech Unimed - BH - Ciência de Dados",
+    "channel": "DigitalInnovationOne",
+    "views": "2,1 mil visualizações",
+    "time": "Transmitido há 2 dias"
+  },
+  {
+    "id": 2,
+    "thumbnail": "./assets/images/thumbnail2.png",
+    "title": "Scrum Talks",
+    "channel": "DigitalInnovationOne",
+    "views": "1,6 mil visualizações",
+    "time": "Transmitido há 3 dias"
+  },
+  {
+    "id": 3,
+    "thumbnail": "./assets/images/thumbnail3.png",
+    "title": "Machine Learning Avançado - Fast Track",
+    "channel": "DigitalInnovationOne",
+    "views": "1,7 mil visualizações",
+    "time": "Transmitido há 4 dias"
+  },
+  {
+    "id": 4,
+    "thumbnail": "./assets/images/thumbnail4.png",
+    "title": "Global Skills #2 - Project Planning",
+    "channel": "DigitalInnovationOne",
+    "views": "971 visualizações",
+    "time": "Transmitido há 5 dias"
+  },
+  {
+    "id": 5,
+    "thumbnail": "./assets/images/thumbnail5.png",
+    "title": "Como aprender a programar Python do ZERO",
+    "channel": "DigitalInnovationOne",
+    "views": "711 visualizações",
+    "time": "Transmitido há 6 dias"
+  }
+]
